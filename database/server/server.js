f2c12f7936bd6a2800a5d6e0dad86ac7e822f9d8
HariisDermawan <harisdermawaan.id@gmail.com>
2023-12-16 01:17:04 +0700

    first commit

--- database/server/server.js
+++ database/server/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongodb = require('mongodb');
 const app = express();
 const port = 3001;
+
+const MongoClient = mongodb.MongoClient;
+const url = "mongodb://localhost:27017/senibudaya";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database connected!");
+  db.close();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
